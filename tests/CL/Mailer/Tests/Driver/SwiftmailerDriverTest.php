55f12df4b1c73bf34fbc0a26f95d180667c91600
Cas Leentfaar <info@casleentfaar.com>
2017-03-13 21:32:35 +0100

    fixed test

--- tests/CL/Mailer/Tests/Driver/SwiftmailerDriverTest.php
+++ tests/CL/Mailer/Tests/Driver/SwiftmailerDriverTest.php
@@ -4,7 +4,10 @@ namespace CL\Mailer\Tests\Driver;
 
 use CL\Mailer\Driver\SwiftmailerDriver;
 use CL\Mailer\Message\Address;
-use CL\Mailer\Message\MessageBuilder;
+use CL\Mailer\Message\MessageBody;
+use CL\Mailer\Message\MessageBodyInterface;
+use CL\Mailer\Message\MessageHeader;
+use CL\Mailer\Message\MessageHeaderInterface;
 use CL\Mailer\Message\Part\HtmlPart;
 use CL\Mailer\Message\ResolvedMessage;
 use PHPUnit\Framework\TestCase;
@@ -41,20 +44,10 @@ class SwiftmailerDriverTest extends TestCase
     {
         $this->swiftmailer->send(Argument::type(Swift_Message::class))
             ->shouldBeCalledTimes(1)
-            ->willReturn(true)
         ;
 
-        $message = new ResolvedMessage(
-            [new Address('from@example.com', 'John From')],
-            [new Address('to@example.com', 'John To')],
-            'Happy birthday!',
-            [],
-            [],
-            [],
-            [new HtmlPart('<p>Sorry I could not make it!</p>', 'utf-8')],
-            []
-        );
+        $message = new ResolvedMessage(new MessageHeader(), new MessageBody());
 
-        $this->assertTrue($this->driver->send($message));
+        $this->driver->send($message);
     }
 }
